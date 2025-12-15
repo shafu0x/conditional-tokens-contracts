b81f22380e9e974b7c621b570ad02886278a9a43
shafu <shafu0x@gmail.com>
2025-12-15 17:27:59 -0500

    trade

--- src/MarketMaker.sol
+++ src/MarketMaker.sol
@@ -4,11 +4,12 @@ pragma solidity 0.8.26;
 import {IERC20} from "forge-std/interfaces/IERC20.sol";
 import {Owned}  from "solmate/auth/Owned.sol";
 
+import {PositionLib}       from "./libraries/PositionLib.sol";
 import {ConditionalTokens} from "./ConditionalTokens.sol";
 import {ConditionId}       from "./interfaces/IConditionalTokens.sol";
 import { Stage }           from "./interfaces/IMarketMaker.sol";
 
-contract MarketMaker is Owned(msg.sender) {
+abstract contract MarketMaker is Owned(msg.sender) {
     ConditionalTokens public conditionalTokens;
     IERC20            public collateralToken;
     uint              public fee;
@@ -58,12 +59,102 @@ contract MarketMaker is Owned(msg.sender) {
     } 
 
     function trade(
-        uint[] memory outcomeTokenAmounts,
-        int           collateralLimit
+        int yesAmount,
+        int noAmount,
+        int collateralLimit
     )
         public
         returns (int netCost)
     {
-        return 0;
+        int outcomeTokenNetCost = calcNetCost(yesAmount, noAmount);
+
+        // TODO: use lib
+        uint absCost = outcomeTokenNetCost >= 0 
+            ? uint(outcomeTokenNetCost)
+            : uint(-outcomeTokenNetCost);
+
+        // TODO: use lib
+        int fees = int(absCost * fee / 1e18);
+
+        netCost = outcomeTokenNetCost + fees;
+
+        // slippage check
+        require(
+            collateralLimit == 0 || netCost <= int(collateralLimit)
+        );
+
+        uint yesPositionId = PositionLib.yesId(collateralToken, conditionId);
+        uint noPositionId  = PositionLib.noId (collateralToken, conditionId);
+
+        // If buying, pull collateral and split
+        if (outcomeTokenNetCost > 0) {
+            require(
+                collateralToken.transferFrom(
+                    msg.sender,
+                    address(this),
+                    uint(outcomeTokenNetCost)
+                )
+            );
+            collateralToken.approve(
+                address(conditionalTokens),
+                uint   (outcomeTokenNetCost)
+            );
+            conditionalTokens.split(
+                conditionId,
+                collateralToken,
+                uint(outcomeTokenNetCost)
+            );
+        }
+
+        if (yesAmount < 0 ) {
+            conditionalTokens.transferFrom(
+                msg.sender,
+                address(this),
+                yesPositionId,
+                uint(-yesAmount)
+            );
+        }
+        if (noAmount < 0) {
+            conditionalTokens.transferFrom(
+                msg.sender,
+                address(this),
+                noPositionId,
+                uint(-noAmount)
+            );
+        }
+
+        if (outcomeTokenNetCost < 0) {
+            conditionalTokens.merge(
+                conditionId,
+                collateralToken,
+                uint(-outcomeTokenNetCost)
+            );
+        }
+
+        if (yesAmount > 0) {
+            conditionalTokens.transfer(
+                msg.sender,
+                yesPositionId,
+                uint(yesAmount)
+            );
+        }
+        if (noAmount > 0) {
+            conditionalTokens.transfer(
+                msg.sender,
+                noPositionId,
+                uint(noAmount)
+            );
+        }
+
+        if (netCost < 0) {
+            require(
+                collateralToken.transfer(
+                    msg.sender,
+                    uint(-netCost)
+                )
+            );
+        }
     }
+
+    function calcNetCost(int yesAmount, int noAmount) public virtual returns (int netCost);
 }
